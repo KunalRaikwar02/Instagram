97ba4fbf1047f72464d103cabd3fa6438b914658
Kunal Raikwar <kunalraikwar12367@gmail.com>
2026-03-13 03:05:03 +0530

    react architecture

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,8 +1,25 @@
 const express = require("express")
 const identifyUser = require("../middlewares/auth.middleware")
 const followController = require("../controllers/follow.controller")
+const userController = require("../controllers/user.controller")
 
 const userRouter = express.Router()
+
+/* 
+  @route POST /api/users/follow/:userid
+  @dec follow a user
+  @access private
+*/
+
+userRouter.post("/follow/:username",identifyUser, userController.followUserController)
+
+/* 
+  @route POST /api/users/unfollow/:userid
+  @dec unfollow a user
+  @access private
+*/
+
+userRouter.post("/unfollow/:username",identifyUser, userController.unfollowUserController)
 
 // Send Follow Request
 userRouter.post("/follow/:username",identifyUser,followController.sendFollowRequest)
